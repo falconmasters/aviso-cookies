b3eb4e676637a736af2ea8dedf976f7614fd1b26
Carlos <carlos@falconmasters.com>
2021-04-29 13:29:14 -0500

    Archivos finales

---
+++ js/aviso-cookies.js
@@ -0,0 +1,21 @@
+const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+const avisoCookies = document.getElementById('aviso-cookies');
+const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+
+dataLayer = [];
+
+if(!localStorage.getItem('cookies-aceptadas')){
+	avisoCookies.classList.add('activo');
+	fondoAvisoCookies.classList.add('activo');
+} else {
+	dataLayer.push({'event': 'cookies-aceptadas'});
+}
+
+botonAceptarCookies.addEventListener('click', () => {
+	avisoCookies.classList.remove('activo');
+	fondoAvisoCookies.classList.remove('activo');
+
+	localStorage.setItem('cookies-aceptadas', true);
+
+	dataLayer.push({'event': 'cookies-aceptadas'});
+});
